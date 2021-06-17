d84d1047a8430992ef921c7dbcf9ddcf818ebda8
Robert Andreev <andreev.robert.a@gmail.com>
2021-06-17 09:30:05 +0300

    parallel

--- src/main/java/mypackage/canvas/MyBezierCanvas.java
+++ src/main/java/mypackage/canvas/MyBezierCanvas.java
@@ -7,7 +7,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.List;
 
-import static java.lang.Math.pow;
+import static java.lang.Math.*;
+import static java.lang.Thread.yield;
 
 
 public class MyBezierCanvas extends Canvas {
@@ -50,6 +51,7 @@ public class MyBezierCanvas extends Canvas {
 
     public void paint(Graphics g) {
         timeEstimated = System.currentTimeMillis();
+        long time = 0;
         Graphics2D g2;
         g2 = (Graphics2D) g;
 
@@ -58,15 +60,48 @@ public class MyBezierCanvas extends Canvas {
             System.out.println(bezier.getX1() + "   " + bezier.getY1());
             System.out.println(bezier.getX2() + "   " + bezier.getY2());
             curve = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-            for (double t = 0; t < 1; t += 0.0002) {
-                Point point = getPoint(bezier, t);
-                System.out.println("x = " + point.getX() + "  y = " + point.getY());
-                curve.setRGB((int) point.getX(), (int) point.getY(), Color.black.getRGB());
+            double step = 50. / (width * height);
+            int availableProcessors = max(Runtime.getRuntime().availableProcessors(), 1);
+            double width = 1. / availableProcessors;
+            long time1 = System.currentTimeMillis();
+            for (double proc = 0; proc < 1; proc += width) {
+                new Drawer(step, proc, min(proc + width, 1), curve, bezier).run();
             }
+            yield();
+            time1 = System.currentTimeMillis() - time1;
+            time += time1;
             g2.drawImage(curve, null, null);
         }
         timeEstimated = System.currentTimeMillis() - timeEstimated;
         if (timeOut != null)
-            timeOut.setText(String.valueOf(timeEstimated));
+            timeOut.setText(String.valueOf(timeEstimated + " " + time));
     }
+
+    class Drawer implements Runnable {
+
+        double step;
+        double t1;
+        double t2;
+        BufferedImage curve;
+        Bezier bezier;
+
+        public Drawer(double step, double t1, double t2, BufferedImage curve, Bezier bezier) {
+            this.step = step;
+            this.t1 = t1;
+            this.t2 = t2;
+            this.curve = curve;
+            this.bezier = bezier;
+            System.out.println("t1 = " + t1 + "  t2 = " + t2 + "  step = " + step);
+        }
+
+        @Override
+        public void run() {
+            for (double t = t1; t <= t2; t += step) {
+                Point point = getPoint(bezier, t);
+                curve.setRGB((int) point.getX(), (int) point.getY(), Color.black.getRGB());
+            }
+        }
+    }
+
+
 }
