11d5efab8e5d00d8daa03de0ec5554811875b568
Robert Andreev <andreev.robert.a@gmail.com>
2021-09-19 23:28:43 +0300

    Make some ui for measure

--- src/main/java/curves/Main.java
+++ src/main/java/curves/Main.java
@@ -1,11 +1,13 @@
 package curves;
 
 import curves.windows.HyperbolaWindow;
+import curves.windows.MeasureWindow;
 import curves.windows.ParabolaWindow;
 
 public class Main {
 
     public static void main(String[] args) {
+        MeasureWindow measureWindow = new MeasureWindow();
         //ParabolaWindow parWindow = new ParabolaWindow();
         //HyperbolaWindow hyperWindow = new HyperbolaWindow();
     }
--- src/main/java/curves/windows/MeasureWindow.java
+++ src/main/java/curves/windows/MeasureWindow.java
@@ -1,39 +1,98 @@
 package curves.windows;
 
+import curves.data.Parameters;
+import curves.drawers.Drawer;
+import curves.drawers.hyperbola.BestHyperbolaDrawer;
+import curves.drawers.parabola.BestParabolaDrawer;
+
 import java.awt.*;
 
 import javax.swing.*;
 
-class JOptionPaneTest {
+public class MeasureWindow {
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setSize(300, 200);
-        String[] items = {"Parabola", "Hyperbola"};
-        JComboBox<String> combo = new JComboBox<>(items);
-        JTextField numberOfDots = new JTextField("1234.56");
-        frame.add(combo);
-        combo.setVisible(true);
-        JTextField minX = new JTextField("9876.54");
-        JTextField maxX = new JTextField("9876.54");
-        JPanel panel = new JPanel(new GridLayout(3, 2));
-        panel.setSize(300, 200);
-//        panel.add(combo);
-        panel.add(new JLabel("Field 1:"));
-        panel.add(numberOfDots);
-        panel.add(new JLabel("Field 2:"));
-        panel.add(minX);
-        panel.setVisible(true);
+    int WINDOW_WIDTH = 500;
+    int WINDOW_HEIGHT = 270;
+
+    JComboBox<String> curveType = new JComboBox<>(new String[] {"Hyperbola", "Parabola"});
+    JLabel curveTypeLabel = new JLabel("Выберите тип кривой");
+    JLabel minXLabel = new JLabel("Минимальное значение X > 0");
+    JTextField minXText = new JTextField();
+    JLabel maxXLabel = new JLabel("Максимальное значение X ");
+    JTextField maxXText = new JTextField();
+    JLabel numberOfDotsLabel = new JLabel("Число точек для построения N > 0");
+    JTextField numberOfDotsText = new JTextField();
+    JLabel numberOfMeasureLabel = new JLabel("Число измерений M > 0");
+    JTextField numberOfMeasureText = new JTextField();
+    JLabel ALabel = new JLabel("Значение параметра A > 0");
+    JTextField AText = new JTextField();
+    JLabel BLabel = new JLabel("Значение параметра B > 0");
+    JTextField BText = new JTextField();
+    JButton button = new JButton("Вычислить время работы");
+    JLabel result = new JLabel("Время работы: ");
+
+    public MeasureWindow()
+    {
+        JFrame frame = new JFrame("Measure Window");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        JPanel panel = new JPanel();
+        panel.setBounds(0,0,WINDOW_WIDTH, WINDOW_HEIGHT);
+        panel.setLayout(new GridLayout(8, 2));
+
+        panel.add(curveTypeLabel);
+        panel.add(curveType);
+        panel.add(minXLabel);
+        panel.add(minXText);
+        panel.add(maxXLabel);
+        panel.add(maxXText);
+        panel.add(numberOfDotsLabel);
+        panel.add(numberOfDotsText);
+        panel.add(numberOfMeasureLabel);
+        panel.add(numberOfMeasureText);
+        panel.add(ALabel);
+        panel.add(AText);
+        panel.add(BLabel);
+        panel.add(BText);
+        panel.add(button);
+        panel.add(result);
+
+        curveType.addItemListener(listener -> {
+            if (listener.getItem().equals("Parabola")) {
+                BText.setEnabled(false);
+            } else {
+                BText.setEnabled(true);
+            }
+        });
+
+        button.addActionListener(listener -> makeMeasure());
+
         frame.add(panel);
         frame.setVisible(true);
-//        int result = JOptionPane.showConfirmDialog(null, panel, "Measure Window",
-//                JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-//        if (result == JOptionPane.OK_OPTION) {
-//            System.out.println(combo.getSelectedItem()
-//                    + " " + numberOfDots.getText()
-//                    + " " + minX.getText());
-//        } else {
-//            System.out.println("Cancelled");
-//        }
     }
+
+    void makeMeasure() {
+        Drawer drawer;
+        double minX = Double.parseDouble(minXText.getText());
+        double maxX = Double.parseDouble(maxXText.getText());
+        int numberOfDots = Integer.parseInt(numberOfDotsText.getText());
+        double A = Double.parseDouble(AText.getText());
+        Parameters parameters = new Parameters(minX, maxX, null, numberOfDots);
+        if (curveType.getSelectedItem().toString().equals("Parabola")) {
+            drawer = new BestParabolaDrawer(parameters, A);
+        } else {
+            double B = Double.parseDouble(BText.getText());
+            drawer = new BestHyperbolaDrawer(parameters, A, B);
+        }
+        long currentTime = System.nanoTime();
+        int numberOfMeasure = Integer.parseInt(numberOfMeasureText.getText());
+        for (int i = 0; i < numberOfMeasure; i++) {
+            System.out.println(drawer.drawCurve());
+        }
+        long measuredTime = System.nanoTime() - currentTime;
+        result.setText("Общее: " + measuredTime / 1000 + " мс. Среднее:" + measuredTime/numberOfMeasure);
+    }
+
 }
